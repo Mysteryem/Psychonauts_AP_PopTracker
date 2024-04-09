a1d26c242bf9a31e681807c0c826c7776a80bcce
Thomas Barlow <github@mysterymayhem.co.uk>
2024-04-09 13:43:15 +0100

    Add brain tumbler map locations and logic

--- scripts/init.lua
+++ scripts/init.lua
@@ -27,6 +27,7 @@ Tracker:AddLocations("locations/BB.json")
 Tracker:AddLocations("locations/BV.json")
 Tracker:AddLocations("locations/MM.json")
 Tracker:AddLocations("locations/LO.json")
+Tracker:AddLocations("locations/BT.json")
 
 -- Layout
 Tracker:AddLayouts("layouts/items.json")
