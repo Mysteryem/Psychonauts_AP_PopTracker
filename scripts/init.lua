675ea1cb5a37d91653d67050243745b14cee5a4c
Thomas Barlow <github@mysterymayhem.co.uk>
2024-04-20 13:18:59 +0100

    Restore item id order
    Saved tracker state uses the ids, but the pack settings items has been
    moved after the yaml settings items.

--- scripts/init.lua
+++ scripts/init.lua
@@ -16,8 +16,8 @@ ScriptHost:LoadScript("scripts/logic/logic.lua")
 
 -- Items
 Tracker:AddItems("items/items.json")
-Tracker:AddItems("items/settings_items.json")
 Tracker:AddItems("items/pack_settings_items.json")
+Tracker:AddItems("items/settings_items.json")
 
 -- Maps
 Tracker:AddMaps("maps/maps.json")
