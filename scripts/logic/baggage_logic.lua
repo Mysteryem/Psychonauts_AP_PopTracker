7ae15e9610d7676b8fae2221af7339a5c3947553
Thomas Barlow <github@mysterymayhem.co.uk>
2026-01-16 00:39:42 +0000

    Count accessible baggage in progressive baggage logic
    This replaces the previous solution that would show all baggage up to
    the current tag count as in-logic if there was any reachable baggage of
    the same type. Now the count is also considered, but sequence-break
    access is not.

--- scripts/logic/baggage_logic.lua
+++ scripts/logic/baggage_logic.lua
@@ -26,3 +26,17 @@ function can_access_any_unfound_baggage_type_in_area(baggage_code, area)
         return AccessibilityLevel.None
     end
 end
+
+function reachable_unfound_hinted_baggage_count(baggage_code)
+    local count = 0
+    for _area, baggage_in_area in pairs(KNOWN_UNFOUND_BAGGAGE) do
+        for _, section in ipairs(baggage_in_area[baggage_code]) do
+            --print("DEBUG: ("..baggage_code..") Checking if "..section.FullID.." is accessible")
+            if section.AccessibilityLevel == AccessibilityLevel.Normal then
+                count = count + 1
+            end
+        end
+    end
+    --print("DEBUG: Counted "..tostring(count).." reachable "..baggage_code)
+    return count
+end
